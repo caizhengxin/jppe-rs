78e2bb58b094914994e7ec2d4bb7f5a5767c875b
jankincai <jankincai12@gmail.com>
2024-03-07 19:48:36 +0800

    :sparkles: feat(common): add http encode/decode example.

---
+++ examples/http_example.rs
@@ -0,0 +1,32 @@
+#![feature(let_chains)]
+use std::collections::HashMap;
+
+use jppe::{BorrowByteDecode, BorrowByteEncode};
+use jppe_derive::{BorrowByteEncode, BorrowByteDecode};
+
+
+#[derive(Debug, Default, PartialEq, Eq, BorrowByteEncode, BorrowByteDecode)]
+pub struct Http<'a> {
+    #[jppe(linend="\x20")]
+    pub method: &'a str,
+    #[jppe(linend="\x20")]
+    pub uri: &'a str,
+    #[jppe(linend="\r\n")]
+    pub http: &'a str,
+    #[jppe(linend="\r\n")]
+    pub headers: HashMap<&'a str, &'a str>,
+}
+
+
+fn main() {
+    let input = b"GET http://www.jankincai.com/ HTTP/1.1\r\nHost: www.jankincai.com\r\nAccept-Encoding: gzip, deflate\r\n";
+    let (input_remain, value) = Http::decode(input, None, None).unwrap();
+    println!("{value:?} {input_remain:?}");
+
+    // encode
+    let mut buf = vec![];
+    value.encode(&mut buf, None, None);
+    // The headers hashmap is out of order and cannot be compared.
+    // assert_eq!(buf, input);
+    assert_eq!(input_remain.is_empty(), true);
+}
